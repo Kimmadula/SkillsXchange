61ba6630ba9e246bb3c100ac4f1092dc09d0506c
Kim Madula <Kim@gmail.com>
2025-10-21 16:00:43 +0800

    feat: implement functional session-end rating system

--- config/ratings.php
+++ config/ratings.php
@@ -2,9 +2,9 @@
 
 return [
     // Master switch to control visibility of all rating-related UI
-    'enabled' => env('RATING_FEATURE_ENABLED', false),
+    'enabled' => env('RATING_FEATURE_ENABLED', true),
 
-    // Future: control which contexts are enabled (session, trade)
+    // Control which contexts are enabled (session, trade)
     'contexts' => [
         'session' => env('RATING_SESSION_ENABLED', true),
         'trade' => env('RATING_TRADE_ENABLED', true),
