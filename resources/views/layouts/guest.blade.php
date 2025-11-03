5f7ddb3a58600a99f10d83c69b62b839eb1c9a08
Johny212003 <Johnnavares7@gmail.com>
2025-11-03 17:42:17 +0800

    Auth UI: compact auth-card for verify email, forgot password, reset password

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -320,7 +320,7 @@
             }
         }
     </style>
-    
+
        <!-- Firebase SDK -->
        <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
        <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
